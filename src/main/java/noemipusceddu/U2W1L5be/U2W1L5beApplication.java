628811d2865e78d819d5287987c8ade4974a8b43
NoemiP94 <m_emi94@hotmail.it>
2024-01-12 10:00:22 +0100

    Initial commit

---
+++ src/main/java/noemipusceddu/U2W1L5be/U2W1L5beApplication.java
@@ -0,0 +1,13 @@
+package noemipusceddu.U2W1L5be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1L5beApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1L5beApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noemipusceddu/U2W1L5be/U2W1L5beApplicationTests.java
@@ -0,0 +1,13 @@
+package noemipusceddu.U2W1L5be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1L5beApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
